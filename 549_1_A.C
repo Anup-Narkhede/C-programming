908f4cf3f5847d019664fba05decdf5f24ce238f
Anup-Narkhede <72505372+Anup-Narkhede@users.noreply.github.com>
2021-04-01 20:58:59 +0530

    Hello World

---
+++ 549_1_A.C
@@ -0,0 +1,14 @@
+    //  Title: 1.Program to display "Hello World"
+    //   Name: Narkhede Anup Anant
+    //Roll no: 549
+
+
+    #include<stdio.h>
+    int main()
+      {
+	clrscr();
+	printf("\t\t\2 Hello World \2");
+	getch();
+	return(0);
+
+       }
